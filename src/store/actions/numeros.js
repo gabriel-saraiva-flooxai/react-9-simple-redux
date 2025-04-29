7bdc0ca894f185286851f31101aa1e4a347de5be
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-29 17:14:57 -0300

    refactor: constants for action types

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,15 +1,19 @@
+import {
+    NUM_MIN_ALTERADO,
+    NUM_MAX_ALTERADO
+} from "./actionTypes"
 
     // Action Creator
 export function altNumMin(novoNum) {
     return {
-        type: 'NUM_MIN_ALTERADO',
+        type: NUM_MIN_ALTERADO,
         payload: novoNum
     }
 }
 
 export function altNumMax(novoNum) {
     return {
-        type: 'NUM_MAX_ALTERADO',
+        type: NUM_MAX_ALTERADO,
         payload: novoNum
     }
 }
--- src/store/reducers/numeros.js
+++ src/store/reducers/numeros.js
@@ -1,3 +1,8 @@
+import {
+    NUM_MIN_ALTERADO,
+    NUM_MAX_ALTERADO
+} from "../actions/actionTypes"
+
 const estadoInicial = {
     min: 1,
     max: 10
@@ -6,12 +11,12 @@ const estadoInicial = {
 export default function(state = estadoInicial, action) {
         
     switch (action.type) {
-        case 'NUM_MIN_ALTERADO':
+        case NUM_MIN_ALTERADO:
             return {
                 ...state,
                 min: action.payload
             }
-        case 'NUM_MAX_ALTERADO':
+        case NUM_MAX_ALTERADO:
             return {
                 ...state,
                 max: action.payload
